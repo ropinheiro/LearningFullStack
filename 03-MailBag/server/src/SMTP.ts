39a09e75f9bd817f8f7a8d2cd7b46c8e23977c8b
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-24 03:28:07 +0100

    MFSD | MailBag Server - SMTP and ServerInfo interface / config

---
+++ 03-MailBag/server/src/SMTP.ts
@@ -0,0 +1,29 @@
+import Mail from "nodemailer/lib/mailer";
+import * as nodemailer from "nodemailer";
+import { SendMailOptions, SentMessageInfo } from "nodemailer";
+import { IServerInfo } from "./ServerInfo";
+
+export class Worker {
+  private static serverInfo: IServerInfo;
+  constructor(inServerInfo: IServerInfo) {
+    Worker.serverInfo = inServerInfo;
+  }
+
+  public sendMessage(inOptions: SendMailOptions): Promise<string> {
+    return new Promise((inResolve, inReject) => {
+      const transport: Mail = nodemailer.createTransport(
+        Worker.serverInfo.smtp
+      );
+      transport.sendMail(
+        inOptions,
+        (inError: Error | null, inInfo: SentMessageInfo) => {
+          if (inError) {
+            inReject(inError);
+          } else {
+            inResolve();
+          }
+        }
+      );
+    });
+  }
+}
