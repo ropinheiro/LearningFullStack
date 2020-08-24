6e3e1dd765ed0679d1d71ef8d6fe8bc8debc4cb6
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-24 06:23:05 +0100

    MFSD | MailBag Server - list, add and delete contacts

---
+++ 03-MailBag/server/src/Contacts.ts
@@ -0,0 +1,58 @@
+import * as path from "path";
+const Datastore = require("nedb");
+
+export interface IContact {
+  _id?: number;
+  name: string;
+  email: string;
+}
+
+export class Worker {
+  private db: Nedb;
+  constructor() {
+    this.db = new Datastore({
+      filename: path.join(__dirname, "contacts.db"),
+      autoload: true,
+    });
+  }
+
+  public listContacts(): Promise<IContact[]> {
+    return new Promise((inResolve, inReject) => {
+      this.db.find({}, (inError: Error, inDocs: IContact[]) => {
+        if (inError) {
+          inReject(inError);
+        } else {
+          inResolve(inDocs);
+        }
+      });
+    });
+  }
+
+  public addContact(inContact: IContact): Promise<IContact> {
+    return new Promise((inResolve, inReject) => {
+      this.db.insert(inContact, (inError: Error | null, inNewDoc: IContact) => {
+        if (inError) {
+          inReject(inError);
+        } else {
+          inResolve(inNewDoc);
+        }
+      });
+    });
+  }
+
+  public deleteContact(inID: string): Promise<string> {
+    return new Promise((inResolve, inReject) => {
+      this.db.remove(
+        { _id: inID },
+        {},
+        (inError: Error | null, inNumRemoved: number) => {
+          if (inError) {
+            inReject(inError);
+          } else {
+            inResolve();
+          }
+        }
+      );
+    });
+  }
+}
