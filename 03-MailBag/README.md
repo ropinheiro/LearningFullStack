56f3a3a7cf1441e17391a600197f845ad27787d2
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-24 05:00:56 +0100

    MFSD | MailBag Server - add endpoint to get mailboxes

--- 03-MailBag/README.md
+++ 03-MailBag/README.md
@@ -1,6 +1,6 @@
 # MailBag
 
-## Local Mail Server Gonfiguratio
+## Local Mail Server Gonfiguration
 (using Greenmail: https://greenmail-mail-test.github.io/greenmail/#deploy_docker_standalone)
 
 **Get Docker image with Greenmail**
@@ -9,3 +9,9 @@
 **Run Docker image**
 > docker run -t -i -p 3025:3025 -p 3110:3110 -p 3143:3143 -p 3465:3465 -p 3993:3993 -p 3995:3995 greenmail/standalone:1.6.0
 > Note that 3025 is for SMTP and 3143 is for IMAP (see URL above for more information)
+
+**Run MailBag server (development mode)**
+> npm run dev
+
+**Test MailBag server**
+> Get mailboxes: curl localhost:8081/mailboxes
--- 03-MailBag/server/src/main.ts
+++ 03-MailBag/server/src/main.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import express, { Express, NextFunction, Request, Response } from "express";
-// import { serverInfo } from "./ServerInfo";
-// import * as IMAP from "./IMAP";
-// import * as SMTP from "./SMTP";
+import { serverInfo } from "./ServerInfo";
+import * as IMAP from "./IMAP";
+import * as SMTP from "./SMTP";
 // import * as Contacts from "./Contacts";
 // import { IContact } from "./Contacts";
 
@@ -27,4 +27,20 @@ app.use(function (
     "Origin,X-Requested-With,Content-Type,Accept"
   );
   inNext();
+});
+
+app.get("/mailboxes", async (inRequest: Request, inResponse: Response) => {
+  try {
+    const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+    const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
+    inResponse.json(mailboxes);
+  } catch (inError) {
+    console.log(inError);
+    inResponse.send("error");
+  }
+});
+
+// Start app listening.
+app.listen(8081, () => {
+  console.log("MailBag server open for requests");
 });
