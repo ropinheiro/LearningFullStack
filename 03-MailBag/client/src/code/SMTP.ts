f93e864806da68887d0ef391b0c4a15e647c1111
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-26 13:37:49 +0100

    MFSD | MailBag Client - add SMTP "mirror"

---
+++ 03-MailBag/client/src/code/SMTP.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+import { config } from './config'
+
+export class Worker {
+  public async sendMessage (
+    inTo: string,
+    inFrom: string,
+    inSubject: string,
+    inMessage: string
+  ): Promise<void> {
+    console.log('SMTP.Worker.sendMessage()')
+
+    await axios.post(`${config.serverAddress}/messages`, {
+      to: inTo,
+      from: inFrom,
+      subject: inSubject,
+      text: inMessage
+    })
+  }
+}
