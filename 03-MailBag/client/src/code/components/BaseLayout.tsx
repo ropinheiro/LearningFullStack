ce0f1c4f02c7243e0f0b766df85c4919c766223d
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-26 20:12:19 +0100

    MFSD | MailBag Client - add dialog for waiting popup

--- 03-MailBag/client/src/code/components/BaseLayout.tsx
+++ 03-MailBag/client/src/code/components/BaseLayout.tsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
 import { createState } from '../state'
 
 class BaseLayout extends Component {
   state = createState(this)
 
   render () {
-    return <div className='appContainer'>{/* TODO */}</div>
+    return (
+      <div className='appContainer'>
+        {
+          <Dialog
+            open={this.state.pleaseWaitVisible}
+            disableBackdropClick={true}
+            disableEscapeKeyDown={true}
+            transitionDuration={0}
+          >
+            <DialogTitle style={{ textAlign: 'center' }}>
+              Bitte warte einen Moment
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                ...Server wird kontaktiert...
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
+
+          /* TODO */
+        }
+      </div>
+    )
   }
 }
 
