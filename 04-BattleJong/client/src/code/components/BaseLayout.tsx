c6e92bf8fc2d238ab627e59ede8f0a8f58cba8fb
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-28 11:55:47 +0100

    MFSD | BattleJong Client - add some structure (main, base layout, css)

---
+++ 04-BattleJong/client/src/code/components/BaseLayout.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class BaseLayout extends Component {
+  render() {
+    return <div>TODO</div>;
+  }
+}
+
+export default BaseLayout;
---
+++ 04-BattleJong/client/src/code/main.tsx
@@ -0,0 +1,9 @@
+import "normalize.css";
+import "../css/main.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import BaseLayout from "./components/BaseLayout";
+
+ReactDOM.render(<BaseLayout />, document.body);
