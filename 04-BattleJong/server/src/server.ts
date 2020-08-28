2aaceba328b37e98ea3a23e7104ec5ff5ef219d9
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-28 10:59:00 +0100

    MFSD | BattleJong Server - add players information placeholder (~game state)

--- 04-BattleJong/server/src/server.ts
+++ 04-BattleJong/server/src/server.ts
@@ -1,5 +1,10 @@
 import path from "path";
 import express, { Express } from "express";
+import WebSocket from "ws";
+
+// When a player connects to the server, we add an object in the form
+// { pid, score: number, stillPlaying: boolean } to this players object.
+const players: any = {};
 
 // Express server
 const app: Express = express();
