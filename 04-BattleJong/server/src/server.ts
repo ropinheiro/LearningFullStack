8f01302f0c30c409f9d5ad2f02592b866ecf3235
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-28 11:06:29 +0100

    MFSD | BattleJong Server - add message handler with basic parser (bare minimum)

--- 04-BattleJong/server/src/server.ts
+++ 04-BattleJong/server/src/server.ts
@@ -18,6 +18,20 @@ const wsServer = new WebSocket.Server({ port: 8080 }, function () {
   console.log("BattleJong WebSocket server ready");
 });
 
+// When a connection happens...
 wsServer.on("connection", (socket: WebSocket) => {
   console.log("Player connected");
+
+  // ...hook up message handler
+  socket.on("message", (inMsg: string) => {
+    console.log(`Message: ${inMsg}`);
+
+    // Format: <message>_<player pid>_<optional data>
+    const msgParts: string[] = inMsg.toString().split("_");
+    const message: string = msgParts[0];
+    const pid: string = msgParts[1];
+    switch (message) {
+      // TODO
+    }
+  });
 });
