5d95e4ba464a92d0c7149d3e4317d16196e9549b
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-08-28 11:16:55 +0100

    MFSD | BattleJong Server - add player done handling

--- 04-BattleJong/server/src/server.ts
+++ 04-BattleJong/server/src/server.ts
@@ -31,14 +31,45 @@ wsServer.on("connection", (socket: WebSocket) => {
     const message: string = msgParts[0];
     const pid: string = msgParts[1];
     switch (message) {
-      // Tile pair is matched (match_<pid>_<points>)
+      // Tile pair is matched
+      // Format: match_<pid>_<points>
       case "match":
         players[pid].score += parseInt(msgParts[2]);
+        // Update all players' scores in all clients
+        // Format: update_<pid>_<points>
         wsServer.clients.forEach(function each(inClient: WebSocket) {
           inClient.send(`update_${pid}_${players[pid].score}`);
         });
+
+      // Player has no more moves / finished
+      // Format: done_<pid>
+      case "done":
+        players[pid].stillPlaying = false;
+        // Check if all players are done...
+        let playersDone: number = 0;
+        for (const player in players) {
+          if (players.hasOwnProperty(player)) {
+            if (!players[player].stillPlaying) {
+              playersDone++;
+            }
+          }
+        }
+        // ...yes they are, so check who won
+        if (playersDone === 2) {
+          let winningPID: string;
+          const pids: string[] = Object.keys(players);
+          if (players[pids[0]].score > players[pids[1]].score) {
+            winningPID = pids[0];
+          } else {
+            winningPID = pids[1];
+          }
+          // Tell all players: game is over and the winner is...
+          // Format: gameOver_<winning pid>
+          wsServer.clients.forEach(function each(inClient: WebSocket) {
+            inClient.send(`gameOver_${winningPID}`);
+          });
+        }
         break;
-      // TODO: other cases
     }
   });
 });
